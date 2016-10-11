02512ab11b86d41c14a59309798e9123f52c4534
Caitrin ARMSTRONG <carmst16@enterprise.CS.McGill.CA>
2016-10-11 18:05:27 -0400

    first structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="network_modularity",
+      version=0.1,
+      description="Returns common modularity scores given an edgelist"
+      url="",
+      author="Caitrin  Armstrong"
+      author_email="caitrin.armstrong@mail.mcgill.ca"
+      licence="MIT"
+      packages=["network_modularity"]
+      zip_safe=False)
